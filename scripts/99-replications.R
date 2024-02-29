c90ba8f9ff5f697361cf81813296e1187e906b83
SEHB2012 <117024975+SEHB2012@users.noreply.github.com>
2024-02-29 10:05:42 -0500

    spotify

--- scripts/99-replications.R
+++ scripts/99-replications.R
@@ -1,7 +1,7 @@
 #### Preamble ####
-# Purpose: Replicated graphs from... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from [...UPDATE THIS...]
+# Author: Sehar Bajwa
+# Date: 29 February 2024 [...UPDATE THIS...]
 # Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
@@ -10,10 +10,22 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(spotifyr)
+
+#### Download data ####
+NASA <-
+  GET("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2024-02-29")
+
+content(NASA)
+content(NASA)$date
+
+knitr::include_graphics(content(NASA)$url)
+
+
+
+#### Save data ####
 # [...UPDATE THIS...]
+# change the_raw_data to whatever name you assigned when you downloaded it.
+write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
-#### Load data ####
-# [...ADD CODE HERE...]
-
-
-
+         
